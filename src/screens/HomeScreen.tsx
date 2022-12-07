ede370f7702a8841d725e8ca3eb409003f2871e3
Fernando Pachon <josep3194@gmail.com>
2022-12-07 09:26:39 -0500

    feat:add homeScreen,navigator and flatlist

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,19 +1,45 @@
-import React from 'react'
-import { View,Text,StyleSheet } from 'react-native'
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import {FlatList} from 'react-native-gesture-handler';
+import Icon from 'react-native-vector-icons/Ionicons';
 
+interface MenuItem{
+  name: string,
+  icon: string,
+  component: string,
+}
 
+const menuItems = [
+  {
+    name: 'Animation 101',
+    icon: 'cube-outline',
+    component: 'Animation101Screen',
+  },
+];
 const HomeScreen = () => {
+
+  const renderMenuItem = (menuItem:MenuItem)=>{
+    return (
+      <View>
+          <Text>{menuItem.name}-{menuItem.icon}</Text>
+      </View>
+    )
+  }
+
   return (
-<View style={styles.container}>
-     <Text>HomeScreen </Text>
-</View>
- )
+    <View style={styles.container}>
+      <FlatList 
+      data={menuItems} 
+      renderItem={({item}) => renderMenuItem(item)}
+      keyExtractor={(item)=>item.name}
+       />
+    </View>
+  );
 };
 
-
 const styles = StyleSheet.create({
-    container: {
+  container: {
     flex: 1,
-    },
+  },
 });
 export default HomeScreen;
