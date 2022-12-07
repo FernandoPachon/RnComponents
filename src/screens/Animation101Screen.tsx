641aea8d7e629f58c6e96e75054e793a674ee3e3
Fernando Pachon <josep3194@gmail.com>
2022-12-07 14:30:34 -0500

    feat:add animated fadein and fadeout

--- src/screens/Animation101Screen.tsx
+++ src/screens/Animation101Screen.tsx
@@ -1,16 +1,46 @@
 import React, { useRef} from 'react'
-import { View,Text,StyleSheet, Animated } from 'react-native'
+import { View,Text,StyleSheet, Animated,Button } from 'react-native'
 
 
 export const Animation101Screen = () => {
-const opacity=useRef( new Animated.Value(0.4)).current;
+const opacity=useRef( new Animated.Value(0)).current;
+
+const fadeIn=()=>{
+    Animated.timing(
+        opacity,
+        {
+            toValue:1,
+            duration:300,
+            useNativeDriver:true
+        }
+    ).start();
+}
+const fadeOut=()=>{
+    Animated.timing(
+        opacity,
+        {
+            toValue:0.4,
+            duration:300,
+            useNativeDriver:true
+        }
+    ).start();
+}
   return (
 <View style={styles.container}>
 <Animated.View style={{
     ...styles.redBox,
     opacity: opacity
     }}/>
-     <Text>Animation101Screen </Text>
+     <Button
+     title="FadeIn"
+     onPress={fadeIn}
+     />
+     <View style={{margin:5}}/>
+     <Button
+    
+     title="FadeOut"
+     onPress={fadeOut}
+     />
 </View>
  )
 };
